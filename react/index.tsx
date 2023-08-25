478e486fe9af351b6b658e217f4416af0dea88f5
Darryl García <darrylgarcia96@gmail.com>
2023-08-25 18:34:12 +0200

    refactor index.tsx

--- react/index.tsx
+++ react/index.tsx
@@ -1,40 +1,16 @@
-import { canUseDOM } from 'vtex.render-runtime'
-import { useRenderSession, useUpdateSession } from 'vtex.session-client'
+import { useUpdateSession } from 'vtex.session-client'
 
 
 import type { PixelMessage } from './typings/events'
 
 export function handleEvents(e: PixelMessage) {
+  const updateSession = useUpdateSession();
 
-  const { session } = useRenderSession()
+  updateSession({
+    variables: {
 
-  console.log({ session })
+      Checkout: { sameSite: "None" }
 
-  const updateSession = useUpdateSession()
-
-        updateSession({
-
-          variables: {
-
-            Checkout: { 'sameSite'= 'None')
-
-          }
-
-
-  switch (e.data.eventName) {
-    case 'vtex:pageView': {
-      const pageTitle = e.data.pageTitle;
-      console.log(`Page view event: ${pageTitle}`);
-      break
     }
-
-    default: {
-      console.log(`Unrecognized event received: ${e.data.eventName}`);
-      break
-    }
-  }
-}
-
-if (canUseDOM) {
-  window.addEventListener('message', handleEvents)
+  })
 }
