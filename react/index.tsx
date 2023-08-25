6498d725ce1c36f54bfe8857a5632d426a183d29
Darryl García <darrylgarcia96@gmail.com>
2023-08-25 15:57:24 +0200

    added sameSite none to session checkout

--- react/index.tsx
+++ react/index.tsx
@@ -1,14 +1,35 @@
 import { canUseDOM } from 'vtex.render-runtime'
+import { useRenderSession, useUpdateSession } from 'vtex.session-client'
+
 
 import type { PixelMessage } from './typings/events'
 
 export function handleEvents(e: PixelMessage) {
+
+  const { session } = useRenderSession()
+
+  console.log({ session })
+
+  const updateSession = useUpdateSession()
+
+        updateSession({
+
+          variables: {
+
+            Checkout: { 'sameSite'= 'None')
+
+          }
+
+
   switch (e.data.eventName) {
     case 'vtex:pageView': {
+      const pageTitle = e.data.pageTitle;
+      console.log(`Page view event: ${pageTitle}`);
       break
     }
 
     default: {
+      console.log(`Unrecognized event received: ${e.data.eventName}`);
       break
     }
   }
